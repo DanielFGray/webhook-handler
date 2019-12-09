4855ce9706d9ea8e79d66fa48c3e531d198b3b0c
DanielFGray <DanielFGray@gmail.com>
2019-12-08 22:45:05 -0600

    fix linting and remove old stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,8 @@
-const config = require('./config')
-
-const globals = Object.keys(config)
-  .reduce((p, k) => Object.assign(p, { [`__${k}`]: false }), { '__non_webpack_require__': false })
-
 module.exports = {
-  parser: 'babel-eslint',
-  extends: [
-    'airbnb',
-  ],
   env: {
-    browser: true,
+    "node": true,
   },
-  globals,
+  extends: ["airbnb-base"],
   rules: {
     semi: ['error', 'never'],
     indent: ['error', 2, { flatTernaryExpressions: true }],
@@ -20,7 +11,5 @@ module.exports = {
     'arrow-parens': ['error', 'as-needed'],
     'space-unary-ops': ['error', { overrides: { '!': true } }],
     'no-unused-vars': ['error', { argsIgnorePattern: '^_$' }],
-    'react/destructuring-assignment': 'off',
-    'react/prop-types': 'off',
-  },
+  }
 }
